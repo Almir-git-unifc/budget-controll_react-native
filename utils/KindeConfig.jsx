439c809816091f778de9ec1b020d77a3f4847607
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2025-05-19 21:36:57 -0300

    Update KindeConfig.jsx
    update file to security

--- utils/KindeConfig.jsx
+++ utils/KindeConfig.jsx
@@ -3,7 +3,7 @@ import { KindeSDK } from '@kinde-oss/react-native-sdk-0-7x';
 export const client = new KindeSDK(
   'https://userkinde.kinde.com',
   'exp://192.168.15.99:8081',  // local
-  '895d87a6fhabs4ba9a65713907fe4927b', // simulate
+  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // simulate
   'exp://192.168.15.99:8081', // local
   'profile email offline openid'
 );
